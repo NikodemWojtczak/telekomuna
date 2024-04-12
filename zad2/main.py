fdffec9acc1f78bfeef24ecd3e47811b2122948a
kjasamil <kamil200202@gmail.com>
2024-04-12 12:53:12 +0200

    crc16 + sending procedure

--- zad2/main.py
+++ zad2/main.py
@@ -38,11 +38,18 @@ def read_file(filename):
         return None
 
 
-def get_128_bit_block(binary_data, block_index):
-    start = block_index * 16
-    end = start + 16
-    block = binary_data[start:end]
-    return block
+def split_data(bytes_array):
+    blocks = []
+    num_blocks = len(bytes_array) // 128
+    for block_nr in range(num_blocks):
+        block = bytearray()
+        for b in range(128):
+            if 128*block_nr+b < len(bytes_array):
+                block.append(bytes_array[128*block_nr+b])
+            else:
+                block.append(0)
+        blocks.append(block)
+    return blocks
 
 
 def calculate_checksum(block):
@@ -52,17 +59,70 @@ def calculate_checksum(block):
     return checksum
 
 
-def calculate_crc16(block):
-    polynomial = 0x1021
-    # algorytm crc16 dla nadajnika do zaimplementowania
+def calculate_crc16(data):
+    crc = 0
+    msb = crc >> 8
+    lsb = crc & 255
+    for b in data:
+        x = b ^ msb
+        x ^= (x >> 4)
+        msb = (lsb ^ (x >> 3) ^ (x << 4)) & 255
+        lsb = (x ^ (x << 5)) & 255
+    return (msb << 8) + lsb
 
 
+def divide_crc16(crc):
+    first_byte = (crc >> 8) & 0xFF
+    second_byte = crc & 0xFF
+    return [first_byte, second_byte]
 
 
+def prepare_packet(data, packet_nr, mode):
+    header = bytearray()
+    header.append(int.from_bytes(SOH, byteorder='big'))
+    header.append(packet_nr)
+    header.append(255 - packet_nr)
+    full = header + data
+    if mode == C:
+        crc = calculate_crc16(data)
+        crc = divide_crc16(crc)
+        full.append(crc[0])
+        full.append(crc[1])
+    elif mode == NAK:
+        full.apend(calculate_checksum(data))
+    return full
 
 
+def send_packet(data, packet_nr, mode):
+    while True:
+        packet = prepare_packet(data, packet_nr, mode)
+        PORT.write(packet)
+        PORT.flush()
+        answer = PORT.read()
+        if answer == ACK:
+            break
 
 
+def send_file(filename):
+    file_bytes = read_file(filename)
+    file_blocks = split_data(file_bytes)
+    packet_nr = 1
+    PORT.flush()
+    initial_answer = PORT.read()
+    while initial_answer != C and initial_answer != NAK:
+        PORT.flush()
+        initial_answer = PORT.read()
+    mode = initial_answer
+    for block in file_blocks:
+        PORT.flush()
+        send_packet(block, packet_nr, mode)
+        print("Wysłano pakiet nr", packet_nr)
+        packet_nr += 1
+        if packet_nr > 255:
+            packet_nr = 0
+    PORT.write(EOT)
+    if PORT.read() == ACK:
+        print("Odebrano potwierdzenie od odbiorcy, dane zostały przesłane poprawnie.")
 
 
 
